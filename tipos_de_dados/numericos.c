83421b67f886aa0449833afffc22b23e3f30deae
Eduardo-Paris <edubp2512@gmail.com>
2025-03-27 20:13:31 -0300

    NOVOpesoalturaidade

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -1,7 +1,25 @@
 #include <stdio.h> 
 
 int main(int agrc, char* argv[]){
-    int idade = 20; 
-    printf("A idade eh %d", idade );
+    int idade; 
+    float altura;
+    float peso;
+
+    printf ("Digite idade:");
+    scanf("%d", &idade);
+
+    printf ("Digite altura:");
+    scanf("%f", &altura);
+
+    printf ("Digite peso:");
+    scanf("%f", &peso);
+
+    printf("A idade eh %d\n", idade);
+    printf("A altura eh %.2f\nO peso eh %.2f", altura, peso );
+
+
+
+
+
     return 0;
-}
+} 
