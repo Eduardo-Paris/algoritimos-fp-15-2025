4ca1ff9fd89bb6048d9c13f33dc62e3c2dc36f89
Eduardo-Paris <edubp2512@gmail.com>
2025-03-28 11:50:19 -0300

    testupdate2

---
+++ tipos_de_dados/operadores.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+
+float n1, n2;
+
+printf("Digite o valor de n1 mano:");
+scanf("%f", &n1);
+
+
+printf("Digite o valor de n2 mano:");
+scanf("%f", &n2);
+
+
+
+
+// adição 
+float soma = n1 + n2; 
+printf("%f + %f = %f",  n1, n2, soma);
+
+
+//subtração
+
+// Multiplicação
+
+//Divisão 
+
+// Potenciação
+
+// Radiciação
+
+//Resto da divisão
+
+//Quociente da divisão
+
+// Incremento
+
+//
+
+
+
+
+
+}
