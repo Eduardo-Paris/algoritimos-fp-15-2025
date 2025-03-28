d5176073de9a8597ac4e7a597b6be524f0f0d396
Eduardo-Paris <edubp2512@gmail.com>
2025-03-28 12:28:29 -0300

    tesupdate2

--- tipos_de_dados/operadores.c
+++ tipos_de_dados/operadores.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+
 
 int main(int argc, char* argv[]){
 
@@ -16,20 +18,33 @@ scanf("%f", &n2);
 
 // adição 
 float soma = n1 + n2; 
-printf("%f + %f = %f",  n1, n2, soma);
-
+printf("%.2f + %.2f = %.2f\n ",  n1, n2, soma);
 
 //subtração
+float diferenca = n1 - n2;
+printf("%.2f - %.2f = %.2f\n",  n1, n2, diferenca);
 
 // Multiplicação
 
+float produto = n1 * n2;
+printf("%.2f * %.2f = %.2f\n",  n1, n2, produto);
+
 //Divisão 
+float quociente = n1 / n2;
+printf("%.2f / %.2f = %.2f\n",  n1, n2, quociente);
 
 // Potenciação
+float potencia = powf (n1, n2);
+printf("%.2f^%.2f = %.2f\n",  n1, n2, potencia);
+
 
 // Radiciação
+float raiz = sqrtf(n1);
+printf("%.2f^1/2 = %.2f\n",  n1, raiz);
 
 //Resto da divisão
+int resto = (int) n1 % (int) n2; 
+printf("%.0f %% %.0f = %.d\n",  n1, n2, resto);
 
 //Quociente da divisão
 
