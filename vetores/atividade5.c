d31538ca7060c0f93ebcb2f2628c069e21962b04
Eduardo-Paris <edubp2512@gmail.com>
2025-04-11 12:16:27 -0300

    atualiz.dia11

---
+++ vetores/atividade5.c
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+
+int main(int agrc, char* argv[]){
+
+
+
+ printf("--------------------------------------------------------------------------\n");
+        
+        printf("-CONTROLE DE NOTAS-      -aperte X pra reiniciar-\n");
+    
+    printf("--------------------------------------------------------------------------\n");
+        
+float n1, n2, notas, n3;
+
+printf("Escreva o numero de alunos:");
+scanf("%f", &n1);
+
+
+printf("Adicione uma nota:");
+scanf("%f", &n2);
+
+float notas = 0;
+float n3 = notas;
+float notas = n2 + n3;
+
+
+printf("Nota[%d] %.2f\n", i,  notas[i])
+
+
+
+    return 0;
+} 
