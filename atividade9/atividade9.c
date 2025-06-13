e2297a17aeb7af053ab8a2835e2aae32fbf4e342
Eduardo-Paris <edubp2512@gmail.com>
2025-06-13 11:55:29 -0300

    sincrt

--- atividade9/atividade9.c
+++ atividade9/atividade9.c
@@ -1,12 +1,42 @@
-int z = 1;
-    int w = 2;
-    incrementaX(&z);
-    
-    printf("y = %d\n", y);
-    printf("z = %d\n", z);
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
 
-    printf("*z = %p\n", &z);
-    printf("*w = %p\n", &w);
 
+int main(int argc, char* argv[]){
+
+
+int menor, maior, esperado, sorteado;
+FILE *log_txt = fopen("log.txt","a");
+menor = atoi (argv[1]);
+maior = atoi(argv[2]);
+esperado = atoi(argv[3]);
+
+strand((unsigned)time(NULL));
+
+if(menor >= maior){
+printf("ARGUMENTO INVALIDO\n");
+return 1;
+}
+
+
+if(esperado < menor || esperado > maior){
+printf("ARGUMENTO INVALIDO");
+return 1;
+
+}
+
+sorteado = (rand() % (maior - menor + 1)) + menor;
+
+if(esperado == sorteado){
+    fprintf(log_txt, "sorteado\n"); 
+}
+else{
+    fprintf(log_txt, "nao sorteado\n");
+}
+  fprintf(log_txt, "minimo=%d maximo=%d esperado=%d sorteado=%d\n", menor, maior, esperado, sorteado);
+
+    fclose(log_txt);
     return 0;
 }
